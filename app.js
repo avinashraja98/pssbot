cc8b129e038f42db816cd72e60cccf646a183057
Avinash Raja <avinashraja98@gmail.com>
2018-02-11 13:10:22 -0330

    added discord code

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 'use strict';
 
-console.log('Hello world');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(8080);
+client.login('NDEyMDMwNDI1ODcyMjAzNzg3.DWH-JA.9kNiLRB5vr2EiJl51aqw0cg-oP0');
+
+client.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+
+    if (message.content === '!pss') {
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voiceChannel) {
+            const connection = await message.member.voiceChannel.join().then(connection => {
+                const dispatcher = connection.playFile('./Audio/clip.mp3');
+                dispatcher.on("end", end => {
+                    voiceChannel.leave();
+                });
+            }).catch(err => console.log(err));
+            isReady = true;
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+});
